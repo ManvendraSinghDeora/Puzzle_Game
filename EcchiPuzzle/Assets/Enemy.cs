bdc1ce67b618463533709b155abdd5f12ecc5810
SumeetDevkai <47910905+SumeetDevkai@users.noreply.github.com>
2020-08-07 14:32:34 +0530

    Main project

---
+++ EcchiPuzzle/Assets/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    int max_Health;
+    int current_Health;
+
+    void Start()
+    {
+        max_Health = 30;
+        current_Health = max_Health;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        current_Health -= damage;
+        if(current_Health <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        print("Lmao,Noob Died");
+        Destroy(gameObject);
+    }
+
+}
