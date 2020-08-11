68332c5142af4b95e2fa38982fdb8844eaf3c338
kshitizvyas <48061180+kshitizvyas@users.noreply.github.com>
2020-08-11 22:18:04 +0530

    arrowtrap

---
+++ EcchiPuzzle/Assets/Scripts/TrapScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapScript : MonoBehaviour
+{
+    public GameObject arrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            arrow.GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
+        if (collision.tag == "arrow")
+        {
+            Destroy(collision.gameObject);
+            RespawnArrow();
+        }
+
+    }
+
+    void RespawnArrow()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
